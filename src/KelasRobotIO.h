25c2cacb4b6965e975e44d79a8fb77e5fb998024
Ponco Nugrah Wibowo <noeke7236@gmail.com>
2024-12-30 21:47:11 +0700

    Add files via upload

---
+++ src/KelasRobotIO.h
@@ -0,0 +1,19 @@
+#ifndef KelasRobotIO_h
+#define KelasRobotIO_h
+
+#include "Arduino.h"
+
+void setupPin(byte numberPin, boolean statusMode){
+    pinMode(numberPin, statusMode);
+}
+void outputSet(byte numberPin, boolean statusPin){
+    digitalWrite(numberPin, statusPin);
+}
+void blink(byte numberPin, int delayBlink){
+    digitalWrite(numberPin, HIGH);
+    delay(delayBlink);
+    digitalWrite(numberPin, LOW);
+    delay(delayBlink);
+}
+
+#endif
